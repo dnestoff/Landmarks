7f0fca638c1ded8aae2064ca27f5283f35db2bc3
Dave Nestoff <d.nestoff@gmail.com>
2021-11-30 15:51:33 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Third User on 11/30/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
