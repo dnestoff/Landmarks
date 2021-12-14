2f78ecf0a92a4f28a6402eda1162680234bdfa6b
Dave Nestoff <d.nestoff@gmail.com>
2021-12-14 16:05:14 -0700

    Track the page in SwiftUI view's state with currentPage @State property

--- Landmarks/Views/PageView/PageViewController.swift
+++ Landmarks/Views/PageView/PageViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 // The page view controller stores an array of Page instances, which must be a type of View. These are the pages you use to scroll between landmarks
 struct PageViewController<Page: View>: UIViewControllerRepresentable {
     var pages: [Page]
+    @Binding var currentPage: Int
     
     // SwiftUI calls makeCoordinator() before makeUIViewController(context:), so you have access to the coordinator object when configuring view controller
         // Tip: use this coordinator to implement common Cocoa patterns, such as delegates, data sources, and responding to user events via target-action
@@ -24,19 +25,22 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
         let pageViewController = UIPageViewController(
             transitionStyle: .scroll,
             navigationOrientation: .horizontal)
-        // Add coordinator as the data source of the UIPageViewController
+        // Add coordinator as the data source and delegate for the UIPageViewController
+            // With the binding connected in both directions, the text view updates to show the correct page number after each swipe
         pageViewController.dataSource = context.coordinator
+        pageViewController.delegate = context.coordinator
 
         return pageViewController
     }
 
     // Add an updateUIViewController(_:context:) method that calls setViewControllers(_:direction:animated:) to provide a view controller for display
+        // passes the value of the currentPage binding
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
         pageViewController.setViewControllers(
-            [context.coordinator.controllers[0]], direction: .forward, animated: true)
+            [context.coordinator.controllers[currentPage]], direction: .forward, animated: true)
     }
     
-    class Coordinator: NSObject, UIPageViewControllerDataSource {
+    class Coordinator: NSObject, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
         var parent: PageViewController
         // Initialize an array of controllers in the coordinator using the pages array of views
         var controllers = [UIViewController]()
@@ -71,6 +75,19 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
                 return controllers.first
             }
             return controllers[index + 1]
+        }
+        
+        // Because SwiftUI calls this method whenever a page switching animation completes, you can find the index of the current view controller and update the binding
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
+            didFinishAnimating finished: Bool,
+            previousViewControllers: [UIViewController],
+            transitionCompleted completed: Bool) {
+            if completed,
+               let visibleViewController = pageViewController.viewControllers?.first,
+               let index = controllers.firstIndex(of: visibleViewController) {
+                parent.currentPage = index
+            }
         }
 
     }
