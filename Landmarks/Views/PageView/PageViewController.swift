74be8d01a82076c7dd77eacfb8344fd8279fe2de
Dave Nestoff <d.nestoff@gmail.com>
2021-12-14 15:45:45 -0700

    Add  swipe left and right interactions to PageView, Add coordinator as the data source of the UIPageViewController

--- Landmarks/Views/PageView/PageViewController.swift
+++ Landmarks/Views/PageView/PageViewController.swift
@@ -12,12 +12,20 @@ import UIKit
 struct PageViewController<Page: View>: UIViewControllerRepresentable {
     var pages: [Page]
     
+    // SwiftUI calls makeCoordinator() before makeUIViewController(context:), so you have access to the coordinator object when configuring view controller
+        // Tip: use this coordinator to implement common Cocoa patterns, such as delegates, data sources, and responding to user events via target-action
+    func makeCoordinator() -> Coordinator {
+            Coordinator(self)
+        }
+    
     // makeUIViewController(context:) method that creates a UIPageViewController with the desired configuration
         // SwiftUI calls this method a single time when it’s ready to display the view, and then manages the view controller’s life cycle
     func makeUIViewController(context: Context) -> UIPageViewController {
         let pageViewController = UIPageViewController(
             transitionStyle: .scroll,
             navigationOrientation: .horizontal)
+        // Add coordinator as the data source of the UIPageViewController
+        pageViewController.dataSource = context.coordinator
 
         return pageViewController
     }
@@ -25,6 +33,46 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
     // Add an updateUIViewController(_:context:) method that calls setViewControllers(_:direction:animated:) to provide a view controller for display
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
         pageViewController.setViewControllers(
-            [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+            [context.coordinator.controllers[0]], direction: .forward, animated: true)
     }
+    
+    class Coordinator: NSObject, UIPageViewControllerDataSource {
+        var parent: PageViewController
+        // Initialize an array of controllers in the coordinator using the pages array of views
+        var controllers = [UIViewController]()
+
+        init(_ pageViewController: PageViewController) {
+            parent = pageViewController
+            controllers = parent.pages.map { UIHostingController(rootView: $0) }
+        }
+        
+        // Below two methods establish the relationships between view controllers, allow swiping back and forth between them
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
+            viewControllerBefore viewController: UIViewController) -> UIViewController?
+        {
+            guard let index = controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if index == 0 {
+                return controllers.last
+            }
+            return controllers[index - 1]
+        }
+
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
+            viewControllerAfter viewController: UIViewController) -> UIViewController?
+        {
+            guard let index = controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if index + 1 == controllers.count {
+                return controllers.first
+            }
+            return controllers[index + 1]
+        }
+
+    }
+
 }
